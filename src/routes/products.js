63c4445aa242ae10694e6e4b0a0f72fa2e6a4f50
paola gutierrez <paolagutierzgil@hotmail.com>
2022-08-03 22:19:30 -0500

    Modified create-product endpoint

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,73 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const ProductsService = require("../services/products");
-const { successResponse, errorResponse } = require("../utils");
+const { successResponse, errorResponse, streamUpload } = require("../utils");
 const multer = require("multer");
 const fileUpload = multer();
-const cloudinary = require("cloudinary").v2;
-const streamifier = require("streamifier");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const productsService = new ProductsService();
 
-router.post("/", async (req, res) => {
+router.post("/", fileUpload.single("image"), async (req, res) => {
+
+  //create image on cloudinary
+  result = await streamUpload(req);
+
+  //create product
+  const product = {
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    imageUrl: result.url
+  }
+
   try {
-    const product = req.body;
     let productCreated = await productsService.createProduct(product);
-    const response = productCreated;
-    successResponse(req, res, response);
+    successResponse(req, res, productCreated);
   } catch (error) {
     errorResponse(req, res, error);
   }
 });
 
-//upload image
-
-router.post(
-  "/upload/",
-  fileUpload.single("product"),
-  function (req, res, next) {
-    let streamUpload = (req) => {
-      return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream((error, result) => {
-          if (result) {
-            resolve(result);
-          } else {
-            reject(error);
-          }
-        });
-
-        streamifier.createReadStream(req.file.buffer).pipe(stream);
-      });
-    };
-
-    async function upload(req) {
-      let result = await streamUpload(req);
-      console.log(result);
-      const product = await productsService.getProductsById(req.body.productId);
-      product.imageUrl = result.url;
-      try {
-        await product.save();
-        return res.status(200).json(product);
-      } catch (err) {
-        return res.status(500).json(err);
-      }
-    }
-
-    upload(req);
-  }
-);
 router.get("/", async (req, res) => {
   try {
-    const product = req.query.id;
-    if (id) {
-      const productCreated = await productsService.getProductsById(id);
-      successResponse(req, res, response);
-    } else {
-      const productCreated = await productsService.getProducts();
+    const productId = req.query.id;
+    if (productId) {
+      const productCreated = await productsService.getProductsById(productId);
       successResponse(req, res, productCreated);
+    } else {
+      const productsCreated = await productsService.getProducts();
+      successResponse(req, res, productsCreated);
     }
   } catch (error) {
     errorResponse(req, res, error);
--- src/utils/index.js
+++ src/utils/index.js
@@ -21,3 +21,20 @@ exports.removePassword = function (user) {
 
   return response;
 };
+
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+
+exports.streamUpload = (req) => {
+  return new Promise((resolve, reject) => {
+    let stream = cloudinary.uploader.upload_stream((error, result) => {
+      if (result) {
+        resolve(result);
+      } else {
+        reject(error);
+      }
+    });
+
+    streamifier.createReadStream(req.file.buffer).pipe(stream);
+  });
+};
